bafc5477745b13d20cdf434ff70f23abb5f02582
adisuyash <adityasuyashgupta@gmail.com>
2024-11-14 23:28:07 +0530

    removed unused dependencies

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
 const express = require("express");
-const https = require("https");
 const bodyParser = require("body-parser");
-const request = require("request");
 
 const app = express();
+
+// Middleware to parse incoming request bodies
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public")); // used to show other files
 
-// app.get("/", function (req, res) {
-//     res.sendFile(__dirname + "/index.html");
-// });
+// Serve static files (like CSS, images) from the "public" directory
+app.use(express.static("public"));
 
+// Route to serve the form (GET request)
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html"); // Serve the index.html file
+});
+
+// Route to handle form submission (POST request)
 app.post("/", function (req, res) {
     const fullName = req.body.name;
     const emailAddress = req.body.email;
+
+    // Send a response with the user input
     res.send(`${fullName} has email address = ${emailAddress}`);
 });
 
+// Start the server on port 3000
 app.listen(3000, () => {
     console.log("Server is running on port: 3000");
 });
-
-// const apiKey = "6f8deb7d1eb8af47be094cbb746e9ed0-us12"; // mailchimp API key 
-// const listId = "db125b4cf4"; // mailchimp list id here
